d4a0df2431d330737dcffda4968beaa71de321a9
Marcelo Mallea Lorca <marcelo.mallea@agiled.cl>
2021-12-12 16:55:18 -0300

    fix DocumentoTag

--- src/main/java/cl/mallea/bitacoraCapitan/model/DocumentTag.java
+++ src/main/java/cl/mallea/bitacoraCapitan/model/DocumentTag.java
@@ -1,6 +1,10 @@
 package cl.mallea.bitacoraCapitan.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Table;
 import java.io.Serializable;
@@ -8,6 +12,11 @@ import java.io.Serializable;
 @Entity
 @Table(name = "document_tag", schema = "bitacoracapitan")
 public class DocumentTag implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "document_tag_id")
+    private Long documentTagId;
 
     //@Column(name = "document_tag_id_tag")
     @JoinColumn(name = "document_tag_id_tag" , nullable = false, updatable = false)
