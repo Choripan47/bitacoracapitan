c463da535dda22cff088b8fb5061fb67e48d67d4
Marcelo Mallea Lorca <marcelo.mallea@agiled.cl>
2021-12-12 17:08:22 -0300

    test endpoint

---
+++ src/main/java/cl/mallea/bitacoraCapitan/DocumentoController.java
@@ -0,0 +1,14 @@
+package cl.mallea.bitacoraCapitan;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DocumentoController {
+
+    @GetMapping(value = "/", produces = "application/json")
+    public ResponseEntity<?> getDocument(){
+        return ResponseEntity.ok("Hola");
+    }
+}
